675ea184475f2340a2451e10732d3459ebf50685
Ahmad Safarjalani <ahmadsaf2007@gmail.com>
2026-04-24 12:05:13 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import MyButton from "./component/myButton/MyButton";
+
+const App = () => {
+  return <MyButton />;
+};
+
+export default App;
---
+++ src/component/myButton/MyButton.tsx
@@ -0,0 +1,8 @@
+import "./MyButton.scss";
+const MyButton = () => {
+    return (
+        <button className="my-button">My-button</button>
+    );
+}
+
+export default MyButton;
