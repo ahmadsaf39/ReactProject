5a7f1ee9a9448dee91fd22ba1d7f6fdde1e99c5c
Ahmad Safarjalani <ahmadsaf2007@gmail.com>
2026-04-26 21:53:03 +0300

    Create responsive Navbar component using Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -5,3 +5,16 @@ const App = () => {
 };
 
 export default App;
+
+import Navbar from './components/navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
